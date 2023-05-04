9fcb373c576b83f46bb43f573e232d2bed0866a4
Willhong <hbilly2002@gmail.com>
2023-05-04 23:54:20 +0900

    Init

---
+++ lib/widgets/episode_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+import '../models/webtoon_episode_model.dart';
+
+class Episode extends StatelessWidget {
+  const Episode({
+    super.key,
+    required this.episode,
+    required this.webtoonId,
+  });
+
+  final WebtoonEpisodeModel episode;
+  final String webtoonId;
+
+  onButtonTap() async {
+    await launchUrlString(
+        //convert episode.id to int and add 1 because the api starts from 0
+        "https://comic.naver.com/webtoon/detail?titleId=$webtoonId&no=${int.parse(episode.id) + 1}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onButtonTap,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.green.shade400,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 15,
+                color: Colors.black.withOpacity(0.2),
+                offset: const Offset(10, 10),
+              ),
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                episode.title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const Icon(
+                Icons.chevron_right_rounded,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
